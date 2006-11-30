2862a600f2bad101aea9e605177ef5aba7d9a086
Simon Stiefel <simon@stiefels.net>
2006-11-30 20:00:44 +0000

    initial import of STNKeychainAccess
    git-svn-id: http://stiefels.net/svn/STNKeychainAccess/trunk@4 5e87bd37-f21f-0410-93ab-ca7b9b871d09

---
+++ DemoController.m
@@ -0,0 +1,94 @@
+//
+//  DemoController.m
+//  STNKeychainAccess Demo
+//
+//  Created by Simon Stiefel on 29.11.06.
+//  Copyright 2006 Simon Stiefel. All rights reserved.
+//
+//  $Id$
+//
+//  Redistribution and use in source and binary forms, with or
+//  without modification, are permitted provided that the
+//  following conditions are met:
+//
+//  1. Redistributions of source code must retain the above
+//  copyright notice, this list of conditions and the following
+//  disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above
+//  copyright notice, this list of conditions and the following
+//  disclaimer in the documentation and/or other materials
+//  provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+//  WARRANTIES, INCLUDING, BUT NOTLIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE FREEBSD PROJECT OR CONTRIBUTORS
+//  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+//  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+//  OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+//  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "DemoController.h"
+
+@implementation DemoController
+
+- (void)awakeFromNib
+{
+    kc = [[STNKeychainAccess alloc] init];
+    [statusline setStringValue:@""];
+}
+
+- (IBAction)fetchStoredPassword:(id)sender
+{
+    [oldpassword setStringValue:@""];
+    if ([self checkServiceAndAccountName]) {
+        NSString *password = nil;
+        OSStatus status;
+        [kc setServiceName:[servicename stringValue]];
+        [kc setAccountName:[accountname stringValue]];
+        if (status = [kc getPassword:&password itemReference:nil]) {
+            [statusline setStringValue:[NSString stringWithFormat:@"Error code: %d", status]];
+        } else {
+            [statusline setStringValue:@"Password fetched"];
+            [oldpassword setStringValue:password];
+        }
+    }
+}
+
+- (IBAction)setNewPassword:(id)sender
+{
+    [oldpassword setStringValue:@""];
+    if ([self checkServiceAndAccountName]) {
+        OSStatus status;
+        [kc setServiceName:[servicename stringValue]];
+        [kc setAccountName:[accountname stringValue]];
+        if (status = [kc savePassword:[newpassword stringValue]]) {
+            [statusline setStringValue:[NSString stringWithFormat:@"Error code: %d", status]];
+        } else {
+            [statusline setStringValue:@"Password saved!"];
+        }
+    }
+}
+
+- (BOOL)checkServiceAndAccountName
+{
+    if ([[servicename stringValue] length] == 0 ||
+        [[accountname stringValue] length] == 0) {
+        [statusline setStringValue:[NSString stringWithFormat:@"Please enter service and account name"]];
+        return NO;
+    }
+    return YES;
+}
+
+- (IBAction)quitApplication:(id)sender
+{
+    [NSApp terminate:self];
+}
+
+
+@end
---
+++ STNKeychainAccess.m
@@ -0,0 +1,151 @@
+//
+//  STNKeychainAccess.m
+//  STNKeychainAccess
+//
+//  Created by Simon Stiefel on 01.08.06.
+//  Copyright 2006 Simon Stiefel. All rights reserved.
+//
+//  $Id$
+//
+//  Redistribution and use in source and binary forms, with or
+//  without modification, are permitted provided that the
+//  following conditions are met:
+//
+//  1. Redistributions of source code must retain the above
+//  copyright notice, this list of conditions and the following
+//  disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above
+//  copyright notice, this list of conditions and the following
+//  disclaimer in the documentation and/or other materials
+//  provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+//  WARRANTIES, INCLUDING, BUT NOTLIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE FREEBSD PROJECT OR CONTRIBUTORS
+//  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+//  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+//  OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+//  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "STNKeychainAccess.h"
+#import <Security/Security.h>
+
+
+@implementation STNKeychainAccess
+
+- (id)initWithServiceName:(NSString *)serviceName
+              accountName:(NSString *)accountName
+{
+    self = [super init];
+    if (self != nil) {
+        [self setAccountName:accountName];
+        [self setServiceName:serviceName];
+    }
+    return self;
+}
+
+- (id) init {
+    return [self initWithServiceName:nil accountName:nil];
+}
+
+- (void)setServiceName:(NSString *)serviceName
+{
+    [_serviceName release];
+    _serviceName = [serviceName copy];
+}
+
+- (NSString *)serviceName
+{
+    return _serviceName;
+}
+
+- (void)setAccountName:(NSString *)accountName
+{
+    [_accountName release];
+    _accountName = [accountName copy];
+}
+
+- (NSString *)accountName
+{
+    return _accountName;
+}
+
+- (OSStatus)savePassword:(NSString *)password
+{
+    NSString *tempPassword = nil;
+    SecKeychainItemRef itemRef;
+    OSStatus status;
+    
+    // check if keychain entry already exists
+    if ([self getPassword:&tempPassword
+            itemReference:&itemRef] == noErr) {
+        // entry exists - check if password differs
+        if (! [tempPassword isEqualToString:password]) {
+            
+            // differs - change it
+            SecKeychainAttribute attrs[] = {
+            { kSecAccountItemAttr, [_accountName cStringLength], [_accountName cString] },
+            { kSecServiceItemAttr, [_serviceName cStringLength], [_serviceName cString] } };
+            
+            const SecKeychainAttributeList attributes = { sizeof(attrs) / sizeof(attrs[0]), attrs };
+            
+            status = SecKeychainItemModifyAttributesAndData(itemRef,
+                                                            &attributes,
+                                                            [password cStringLength],
+                                                            [password cString]);
+            if (status != noErr) {
+                NSLog(@"Error while updating keychain data: %d", status);
+            }         
+            return status;
+        } else {
+            // doesn't differ - all ok
+            return noErr;
+        }
+    } else {
+        // no entry - add it
+        return SecKeychainAddGenericPassword(NULL,
+                                             [_serviceName cStringLength],
+                                             [_serviceName cString],
+                                             [_accountName cStringLength],
+                                             [_accountName cString],
+                                             [password cStringLength],
+                                             [password cString],
+                                             NULL);
+    }
+}
+
+- (OSStatus)getPassword:(NSString **)password
+          itemReference:(SecKeychainItemRef *)itemRef
+{
+    void *passwordData;
+    UInt32 passwordLength;
+    
+    OSStatus status = SecKeychainFindGenericPassword(NULL,
+                                                     [_serviceName cStringLength],
+                                                     [_serviceName cString],
+                                                     [_accountName cStringLength],
+                                                     [_accountName cString],
+                                                     &passwordLength,
+                                                     &passwordData,
+                                                     itemRef);
+    
+    if (status != errSecItemNotFound) {
+        if (*password != nil) {
+            [*password release];
+        }
+        *password = [NSString stringWithCString:(char *)passwordData];
+        SecKeychainItemFreeContent(NULL, passwordData);
+    } else {
+        *password = nil;
+    }
+
+    return status;
+}
+
+@end
